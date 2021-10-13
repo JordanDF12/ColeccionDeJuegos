5e0482815018f5a06f9719311f5d5a4434abf2f2
JordanDF12 <jordan.florindez@gmail.com>
2021-10-13 14:48:53 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Mac 10 on 10/13/21.
+//  Copyright © 2021 deah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
